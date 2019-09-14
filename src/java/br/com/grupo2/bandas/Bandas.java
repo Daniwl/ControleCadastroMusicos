ec7a571e40b9e7b8272f66fc2051eea1f8b38671
Felipe <Luisatto@Luisatto>
2019-09-14 00:09:31 -0300

    Criação dos Pacotes

---
+++ src/java/br/com/grupo2/bandas/Bandas.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.grupo2.bandas;
+
+/**
+ *
+ * @author Luisatto
+ */
+public class Bandas {
+    private String musico;
+    private String album;
+    private String musica;
+
+    public String getMusico() {
+        return musico;
+    }
+
+    public void setMusico(String musico) {
+        this.musico = musico;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getMusica() {
+        return musica;
+    }
+
+    public void setMusica(String musica) {
+        this.musica = musica;
+    }
+}
