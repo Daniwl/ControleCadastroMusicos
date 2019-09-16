2f40372fb801463c5f12ef49dbfdaa4c32c52aaa
Daniwl <daniel.ribeiroq@gmail.com>
2019-09-15 22:18:54 -0300

    Corrigindo

--- src/java/br/com/grupo2/Bandas.java
+++ src/java/br/com/grupo2/Bandas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.cadastro;
+package br.com.grupo2;
 
 /**
  *
--- src/java/br/com/grupo2/Db.java
+++ src/java/br/com/grupo2/Db.java
@@ -16,7 +16,7 @@ public class Db {
     private static ArrayList<Musicas> musicas;
     
     
-        public static ArrayList<Discos> getCadastro() {
+       public static ArrayList<Discos> getCadastroMusica() {
             if (cadastrar == null) {
                 cadastrar = new ArrayList<>();
             }
